69b0b1d30abb552384e59689aba0581ed8200a24
jeremy <jeremy@jeremy-desktop>
2015-12-09 09:28:01 +0100

    Ajout du fichier formulaire et formulaire success + function controller + modele + view

---
+++ ProjetOffresStage/src/projetoffresstage/MainProjet.java
@@ -0,0 +1,18 @@
+package projetoffresstage;
+import java.util.ArrayList;
+/**
+ *
+ * @author jeremy
+ */
+public class MainProjet {
+
+    /**
+     * @param args the command line arguments
+     */
+    private ArrayList<String>lesOffres;
+    private ArrayList<Entreprise>lesEntreprises;
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- application/views/reservations/formulaire.php
+++ application/views/reservations/formulaire.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -15,34 +11,45 @@ and open the template in the editor.
         echo validation_errors();
         echo form_open('reservations/ajout_utilisateur');
         ?>
-        
-         <label for="id">Votre id : </label>
-         <input type="text" name="id" value=""/><br />
 
-        <label for="date_arrivee">Date d'arrivée :</label>
-        <input type="date" name="date_arrivee" /><br />
+        <div>
+            <label for="id">Votre id : </label>
+            <input type="text" name="id" value=""/><br />
+        </div>
 
-        <label for="date_depart"> Date de départ : </label>
-        <input type="date" name="date_depart" /><br />
+        <div>
+            <label for="date_arrivee">Date d'arrivée :</label>
+            <input type="date" name="date_arrivee" /><br />
+        </div>
 
-        <label for="nbpersonnes">Nombre de personnes : </label>
-        <input type="text" name="nbpersonnes" value="" /><br />
-        
-        <label>Pension : </label>
-        <select name = "pension">
-            <option value="pension_complete">Pension complete</option>
-            <option value="demi-pension">Demi-pension</option>
-        </select><p><br></p>
-        
-        <label for="forfait">Voulez-vous le forfait "ménage de fin de séjour" ?</label>
-        <input type="radio" name="forfait" value=1 />Oui
-        <input type="radio" name="forfait" value=0 checked/>Non
-        <p><br></p>
+        <div>
+            <label for="date_depart"> Date de départ : </label>
+            <input type="date" name="date_depart" /><br />
+        </div>
 
+        <div>
+            <label for="nbpersonnes">Nombre de personnes : </label>
+            <input type="text" name="nbpersonnes" value="" /><br />
+        </div>
+
+        <div>
+            <label for="pension">Pension : </label>
+            <select name = "pension">
+                <option value="pension_complete">Pension complete</option>
+                <option value="demi-pension">Demi-pension</option>
+            </select><br>
+        </div>
+
+        <div>
+            <label for="forfait">Voulez-vous le forfait "ménage de fin de séjour" ?</label>
+            <input type="radio" name="forfait" value=1 />Oui
+            <input type="radio" name="forfait" value=0 checked/>Non
+            <br>
+        </div>
         <input type="submit" name="submit" value="Réserver !" />
         <?php echo form_close(); ?><br/>
-        
-        
+
+
         <a href="http://localhost/codeigniter/index.php/Reservations/calendrier">Retour au calendrier</a>
     </body>
 </html>
